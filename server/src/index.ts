5071d928bbf67c965bbfe5731a9afe3a1887e1e2
Noble Obioma <nobioma1@gmail.com>
2020-06-11 10:01:45 +0100

    Add Handler function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello World');
+export const handler = async (): Promise<any> => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from aws-chat-randy Lambda!'),
+  };
+  return response;
+};
