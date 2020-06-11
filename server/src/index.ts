516d2c4391b7eefa0c0ad100bd9bcd2298e3941f
Noble Obioma <nobioma1@gmail.com>
2020-06-11 10:40:14 +0100

    Trigger Deploy 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 export const handler = async (): Promise<any> => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from aws-chat-randy Lambda Function!'),
+    body: JSON.stringify('Hello from aws-chat-randy My Lambda Function!'),
   };
   return response;
 };
