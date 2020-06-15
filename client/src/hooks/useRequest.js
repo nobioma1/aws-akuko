07c5ef04ad877b99a6ef5dbfca86b8450228652e
Noble Obioma <nobioma1@gmail.com>
2020-06-15 03:26:39 +0100

    create hook for request

---
+++ client/src/hooks/useRequest.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export const useRequest = ({ method, url }) => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const doRequest = async ({ values, onSuccess }) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url, {
+        method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+
+      const data = await response.json();
+
+      if (onSuccess) {
+        onSuccess(data);
+      }
+
+      setIsLoading(false);
+      return data;
+    } catch (err) {
+      setIsLoading(false);
+      setError(<p>Something went wrong</p>);
+    }
+  };
+
+  return { doRequest, error, isLoading };
+};
